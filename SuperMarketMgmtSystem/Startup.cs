5a28cede7a5b7c88c87c1238cab59db690623227
mcsak <chinnasamym@live.com>
2014-12-27 20:25:10 +0530

    inital project setups

---
+++ SuperMarketMgmtSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SuperMarketMgmtSystem.Startup))]
+namespace SuperMarketMgmtSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
